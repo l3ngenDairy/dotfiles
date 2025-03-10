a45f5645520ef9f0e8554ad6f05c4693ea1c1760
l3ngenDairy <davidhobman@protonmail.com>
2025-03-10 22:08:14 +1300

    edited ollama.nix in modules

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -1,15 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      ollama = super.ollama.override { acceleration = "cuda"; };
-    })
-  ];
-
   environment.systemPackages = with pkgs; [
     ollama
-    ollama-cuda
-                
   ];
 }
