b7f49dd0b2b875a443fb4c56e9c53a8a0db8cfd5
l3ngenDairy <davidhobman@protonmail.com>
2025-04-05 03:00:53 +1300

    d

--- da-desktop/modules/docker.nix
+++ da-desktop/modules/docker.nix
@@ -2,30 +2,28 @@
 {
   virtualisation.docker = {
     enable = true;
-    # Remove enableNvidia since it's deprecated
     rootless = {
       enable = false;
       setSocketVariable = true;
     };
     daemon.settings = {
+      "default-runtime" = "nvidia";
+      "runtimes" = {
+        "nvidia" = {
+          "path" = "nvidia-container-runtime";
+          "runtimeArgs" = [];
+        };
+      };
       "default-address-pools" = [
         { "base" = "172.27.0.0/16"; "size" = 24; }
       ];
-      # Remove the entire runtimes section 
-
-"runtimes" = {
-    "nvidia" = {
-    "path" = "nvidia-container-runtime"; # no pkgs prefix
-    "runtimeArgs" = [];
     };
   };
 
-
-    };
-  };
-  
   users.users.david.extraGroups = [ "docker" ];
+
   environment.systemPackages = with pkgs; [
     docker-compose
   ];
 }
+
