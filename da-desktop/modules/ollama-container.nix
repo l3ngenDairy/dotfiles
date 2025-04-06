ab246e78648f8d19b75fa7ebddc594a6cc06e723
l3ngenDairy <davidhobman@protonmail.com>
2025-04-06 14:16:41 +1200

    removed enableNvidia as it is depresiated

--- da-desktop/modules/ollama-container.nix
+++ da-desktop/modules/ollama-container.nix
@@ -2,12 +2,10 @@
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;  # optional, makes `docker` CLI work
-    enableNvidia = true;  # GPU support
-  };
+     };
   environment.systemPackages = with pkgs; [
     ollama           
   ];
-  
   hardware.nvidia-container-toolkit.enable = true;
 }
 
