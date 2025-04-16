91e782ca5f65b929622254a85f7437905f29634d
l3ngenDairy <davidhobman@protonmail.com>
2025-04-16 20:34:23 +1200

    boot.kernelParams = [ lockdown=confidentiality ];

--- da-desktop/modules/boot.nix
+++ da-desktop/modules/boot.nix
@@ -3,5 +3,7 @@
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
-  boot.kernelPackages = pkgs.linuxPackages_latest;     
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ "lockdown=confidentiality" ];
+        
 }
