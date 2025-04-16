8efa3c3a93929fbc1275ed0fd46f4182d2256d42
l3ngenDairy <davidhobman@protonmail.com>
2025-04-16 21:02:24 +1200

    single gpu prep

--- da-desktop/configuration.nix
+++ da-desktop/configuration.nix
@@ -27,7 +27,16 @@
 
 
   services.fwupd.enable = true;
-  boot.kernelParams = [ "amd_iommu=on" ];     
+
+
+
+        #single gpu passthrough attempt
+
+  boot.kernelParams = [ 
+    "amd_iommu=on"
+    "iommu=pt" 
+  ];     
+  boot.kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" "vfio_virqfd" ];      
           
 
   hardware.enableAllFirmware = true;
