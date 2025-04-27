3c2dbbea0d7e442c1f9e49471bd04bcf64e65a53
l3ngenDairy <davidhobman@protonmail.com>
2025-04-27 22:53:30 +1200

    exec added to drives 1 and 2

--- da-desktop/modules/drives.nix
+++ da-desktop/modules/drives.nix
@@ -6,13 +6,13 @@
     "/media/drive-1" = {
       device = "/dev/disk/by-uuid/61d1bd2e-1784-4aea-a561-7198ae6b6829";
       fsType = "ext4";
-      options = [ "defaults" "nofail" "users" ];
+      options = [ "defaults" "nofail" "users" "exec" ];
     };
 
     "/media/drive-2" = {
       device = "/dev/disk/by-uuid/c54e517e-44cc-4a7e-9230-0905134ee93f";
       fsType = "ext4";
-      options = [ "defaults" "nofail" "users" ];
+      options = [ "defaults" "nofail" "users" "exec" ];
     };
   };
 }
