2ca43fe7c1249cd5afe3d1123e8b0092323f09e5
l3ngenDairy <davidhobman@protonmail.com>
2025-05-04 20:07:07 +1200

    t

--- da-desktop/modules/mouse.nix
+++ da-desktop/modules/mouse.nix
@@ -1,18 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # Enable ratbagd with the correct package
   services.ratbagd = {
     enable = true;
     package = pkgs.libratbag;
   };
 
-  environment.systemPackages = with pkgs; [
-    libratbag
-    piper
-  ];
-
-  # Systemd service to configure the mouse
   systemd.user.services.logitech-g502-config = {
     description = "Configure Logitech G502 HERO Mouse";
     wantedBy = [ "graphical-session.target" ];
@@ -22,40 +15,19 @@
       Type = "oneshot";
       ExecStart = pkgs.writeShellScript "logitech-g502-config" ''
         # Wait for ratbagd to be ready
-        sleep 5  # Increased delay for more reliability
+        sleep 5
 
-        # Use the actual device name from ratbagctl list
         DEVICE="singing-gundi"
 
-        # First reset to default to ensure clean state
+        # First reset to default
         ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 reset
 
-        # Set polling rate
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 rate set 1000
+        # Button remapping - using the correct syntax
+        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 button 5 action set button button6
+        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 button 6 action set button button7
+        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 button 7 action set button button8
 
-        # Configure DPI settings
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 resolution 1 set 2400
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 resolution set 1
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 resolution disable 0
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 resolution disable 2
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 resolution disable 3
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 resolution disable 4
-
-        # Rebind buttons (using correct button numbers from your info output)
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 button 5 action button button6
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 button 6 action button button7
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 button 7 action button button8
-
-        # Disable all other profiles
-        for profile in 1 2 3 4; do
-          ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile $profile disable
-        done
-
-        # Turn off LEDs
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 led 0 set mode off
-        ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 led 1 set mode off
-
-        # Make profile 0 active (in case another profile was active)
+        # Ensure profile 0 is active (your output shows profile 1 is active)
         ${pkgs.libratbag}/bin/ratbagctl "$DEVICE" profile 0 set-active
       '';
     };
